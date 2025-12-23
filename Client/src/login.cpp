c6ff1d0ac6b2a0b1a440e453861a179165132308
Hamdan Tariq <hamdantariqedu@gmail.com>
2025-12-23 13:28:49 +0500

    Update login.cpp

--- Client/src/login.cpp
+++ Client/src/login.cpp
@@ -1,3 +1,16 @@
+/*
+   © 2025 Hamdan Tariq
+
+   LAN Chat App with File Transfer
+
+   Licensed under the Apache License, Version 2.0.
+   You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+   Distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND.
+   
+   File: login.cpp
+   Description: 
+*/
 #include "ui_login.h"
 #include "pch.h"
 #include "login.h"
@@ -182,4 +195,5 @@ void Login::on_reconnectPushButton_clicked()
 {
     emit reconnect();
 }
+
 
