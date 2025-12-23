0ab17428b6e551ae6caa94c7f111e045e8c76e03
Hamdan Tariq <hamdantariqedu@gmail.com>
2025-12-23 13:25:41 +0500

    Add license and description comments to pch.h
    Added license information and file description to pch.h

--- Client/include/pch.h
+++ Client/include/pch.h
@@ -1,3 +1,16 @@
+/*
+   © 2025 Hamdan Tariq
+
+   LAN Chat App with File Transfer
+
+   Licensed under the Apache License, Version 2.0.
+   You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+   Distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND.
+   
+   File: pch.h
+   Description:  
+*/
 #ifndef PCH_H
 #define PCH_H
 
@@ -22,3 +35,4 @@
 #include <QFrame>
 
 #endif // PCH_H
+
