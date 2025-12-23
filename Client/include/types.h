9cfa0aa3656441dc2c90b562114ecec647a530cc
Hamdan Tariq <hamdantariqedu@gmail.com>
2025-12-23 13:26:49 +0500

    Update types.h

--- Client/include/types.h
+++ Client/include/types.h
@@ -1,3 +1,16 @@
+/*
+   © 2025 Hamdan Tariq
+
+   LAN Chat App with File Transfer
+
+   Licensed under the Apache License, Version 2.0.
+   You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+   Distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND.
+   
+   File: types.h
+   Description:  
+   */
 #ifndef TYPES_H
 #define TYPES_H
 #include "olc_net.h"
@@ -23,3 +36,4 @@ enum class MessageType : uint32_t {
 };
 
 #endif // TYPES_H
+
