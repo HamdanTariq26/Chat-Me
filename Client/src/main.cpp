6ce1051161686159120aca24f683d51a8d69fd48
Hamdan Tariq <hamdantariqedu@gmail.com>
2025-12-23 13:29:10 +0500

    Update main.cpp

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -1,10 +1,21 @@
+/*
+   © 2025 Hamdan Tariq
+
+   LAN Chat App with File Transfer
+
+   Licensed under the Apache License, Version 2.0.
+   You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+   Distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND.
+   
+   File: main.cpp
+   Description: 
+*/
+
 #include "mainwindow.h"
 #include "login.h"
 #include "pch.h"
 #include "client.h"
-
-
-
 
 int main(int argc, char *argv[])
 {
@@ -39,3 +50,4 @@ int main(int argc, char *argv[])
     delete networkWorker;
     delete client;
 }
+
